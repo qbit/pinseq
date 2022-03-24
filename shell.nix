a8bfc35f035437ac95b1108d3b9a0a76f189664e
Aaron Bieber <aaron@bolddaemon.com>
2022-03-24 08:11:05 -0600

    initial

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    #buildInputs = [ ];
+    shellHook = ''
+      export PS1='\u@\h:\w; '
+      export NO_COLOR=1
+
+    '';
+    nativeBuildInputs = with pkgs.buildPackages; [
+      nodePackages.typescript
+      nodePackages.yarn
+    ];
+}
